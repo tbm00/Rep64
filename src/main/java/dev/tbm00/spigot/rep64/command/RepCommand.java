2247395cd720986f842bb19855440de6e0205f82
tbm00 <89701900+tbm00@users.noreply.github.com>
2024-07-05 16:03:05 -0500

    Formatted player messages

--- src/main/java/dev/tbm00/spigot/rep64/command/RepCommand.java
+++ src/main/java/dev/tbm00/spigot/rep64/command/RepCommand.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class RepCommand implements TabExecutor {
     private RepManager repManager;
     private String[] subCommands = new String[]{""};
+    private final String prefix = ChatColor.DARK_GRAY + "[" + ChatColor.WHITE + "Rep" + ChatColor.DARK_GRAY + "] " + ChatColor.RESET;
 
     public RepCommand(RepManager repManager) {
         this.repManager = repManager;
@@ -27,16 +28,17 @@ public class RepCommand implements TabExecutor {
         // /rep
         if (args.length == 0) {
             if (!(sender instanceof Player)) {
-                sender.sendMessage(ChatColor.RED + "This command can only be run by a player.");
+                sender.sendMessage(prefix + ChatColor.RED + "This command can only be run by a player.");
                 return true;
             }
             Player initiator = (Player) sender;
             PlayerEntry playerEntry = repManager.getPlayerEntry(initiator.getUniqueId().toString());
             if (playerEntry != null) {
                 double repShown = playerEntry.getRepShown();
-                initiator.sendMessage(ChatColor.GREEN + "Your reputation: " + repShown);
+                initiator.sendMessage(ChatColor.GRAY + "Your reputation: " + ChatColor.LIGHT_PURPLE + String.format("%.1f", repShown) 
+                    + ChatColor.DARK_GRAY + " (avg of " + playerEntry.getRepCount() + " entries)");
             } else {
-                initiator.sendMessage(ChatColor.RED + "An error occurred while fetching your reputation.");
+                initiator.sendMessage(ChatColor.RED + "An error occurred while fetching your reputation!");
             }
         }
 
@@ -46,13 +48,13 @@ public class RepCommand implements TabExecutor {
                 if (sender.hasPermission("rep64.reload")) {
                     try {
                         repManager.reload();
-                        sender.sendMessage(ChatColor.GREEN + "You successfully reloaded the plugin!");
+                        sender.sendMessage(prefix + ChatColor.GREEN + "You successfully reloaded the plugin!");
                     } catch (Exception e){
                         System.out.println("Error connecting database when reloading!");
                     }
                 }
                 else {
-                    sender.sendMessage(ChatColor.RED + "No permission!");
+                    sender.sendMessage(prefix + ChatColor.RED + "No permission!");
                 }
             }
             else {
@@ -61,9 +63,10 @@ public class RepCommand implements TabExecutor {
                 PlayerEntry targetEntry = repManager.getPlayerEntry(repManager.getPlayerUUID(targetName));
                 if (targetEntry != null) {
                     double targetRepShown = targetEntry.getRepShown();
-                    initiator.sendMessage(ChatColor.GREEN + targetName + " reputation: " + targetRepShown);
+                    initiator.sendMessage(ChatColor.GRAY + targetName + " reputation: " + ChatColor.LIGHT_PURPLE + String.format("%.1f", targetRepShown) 
+                        + ChatColor.DARK_GRAY + " (avg of " + targetEntry.getRepCount() + " entries)");
                 } else {
-                    initiator.sendMessage(ChatColor.RED + "An error occurred while fetching their reputation.");
+                    initiator.sendMessage(ChatColor.RED + "An error occurred while fetching their reputation!");
                 }
             }
         }
@@ -71,58 +74,61 @@ public class RepCommand implements TabExecutor {
         // /rep <> <>
         if (args.length == 2) {
             if (!(sender instanceof Player)) {
-                sender.sendMessage(ChatColor.RED + "This command can only be run by a player.");
+                sender.sendMessage(prefix + ChatColor.RED + "This command can only be run by a player!");
                 return true;
             }
             if (!sender.hasPermission("rep64.set")) {
-                sender.sendMessage(ChatColor.RED + "No permission!");
+                sender.sendMessage(prefix + ChatColor.RED + "No permission!");
                 return true;
             }
 
             Player initiator = (Player) sender;
             String targetName = args[0];
-            int rep;
-
-            try {
-                rep = Integer.parseInt(args[1]);
-                if (rep < 0 || rep > 10) {
-                    sender.sendMessage(ChatColor.RED + "The reputation value must be between 0-10.");
-                    return true;
-                }
-            } catch (NumberFormatException e) {
-                sender.sendMessage(ChatColor.RED + "The reputation value must be a number.");
-                return true;
-            }
+            String action = args[1];
+            //int rep;
 
             String targetUUID = repManager.getPlayerUUID(targetName);
             PlayerEntry targetPlayerEntry = repManager.getPlayerEntry(targetUUID);
-            if (targetPlayerEntry != null) {
-                try {
-                    // Re-calculate current
-                    repManager.calculateRepAverage(targetPlayerEntry.getPlayerUUID());
-    
-                    RepEntry targetRepEntry = new RepEntry(initiator.getUniqueId().toString(), targetUUID, rep);
-
-                    // Save/create new rep entry in databases (sql and cache)
-                    repManager.saveRepEntry(targetRepEntry);
-
-                    // Save/create new rep entry in databases (sql and cache)
-                    repManager.savePlayerEntry(targetPlayerEntry);
-                    repManager.calculateRepAverage(targetPlayerEntry.getPlayerUUID());
-    
-                    // Message player
-                    sender.sendMessage(ChatColor.YELLOW + targetPlayerEntry.getPlayerUsername() + "'s Rep: " + targetPlayerEntry.getRepShownLast() + " (avg of " + (targetPlayerEntry.getRepCount()-1) + " entries)");
-                    sender.sendMessage(ChatColor.GREEN + "You have given " + targetRepEntry.getReceiverUUID() + " a reputation of " + targetRepEntry.getRep());
-                    sender.sendMessage(ChatColor.GREEN + targetPlayerEntry.getPlayerUsername() + "'s Rep: " + targetPlayerEntry.getRepShown() + " (avg of " + targetPlayerEntry.getRepCount() + " entries)");
-                } catch (Exception e) {
-                    sender.sendMessage(ChatColor.RED + "An error occurred while creating the reputation entry.");
-                    e.printStackTrace();
+            if (action.equalsIgnoreCase("unset")) {
+                if (targetPlayerEntry != null) {
+                    String initiatorUUID = initiator.getUniqueId().toString();
+                    String receiverUUID = targetPlayerEntry.getPlayerUUID();
+                    repManager.deleteRepEntry(initiatorUUID, receiverUUID);
+                    sender.sendMessage(prefix + ChatColor.GREEN + "You have removed your reputation entry for " + targetName + "!");
+                } else {
+                    sender.sendMessage(prefix + ChatColor.RED + "Error: Player data is not available!");
                 }
             } else {
-                sender.sendMessage(ChatColor.RED + "An error occurred while fetching their data.");
+                try {
+                    int rep = Integer.parseInt(args[1]);
+                    if (rep < 0 || rep > 10) {
+                        sender.sendMessage(prefix + ChatColor.RED + "The reputation value must be between 0-10 (or 'unset')!");
+                        return true;
+                    }
+
+                    // re-calculate current
+                    repManager.calculateRepAverage(targetPlayerEntry.getPlayerUUID());
+                    RepEntry targetRepEntry = new RepEntry(initiator.getUniqueId().toString(), targetUUID, rep);
+
+                    // save/create new rep entry in databases (sql and cache)
+                    repManager.saveRepEntry(targetRepEntry);
+                    repManager.calculateRepAverage(targetPlayerEntry.getPlayerUUID());
+                    
+                    // message player
+                    sender.sendMessage(prefix + ChatColor.GREEN + "You have given " + repManager.getPlayerEntry(targetRepEntry.getReceiverUUID()).getPlayerUsername() 
+                        + " a reputation of " + targetRepEntry.getRep());
+                    sender.sendMessage(prefix + ChatColor.GRAY + targetPlayerEntry.getPlayerUsername() + "'s Rep: " 
+                        + ChatColor.RED + String.format("%.1f", targetPlayerEntry.getRepShownLast())
+                        + ChatColor.DARK_GRAY + " (avg of " + (targetPlayerEntry.getRepCount()-1) + " entries)");
+                    sender.sendMessage(prefix + ChatColor.GRAY + targetPlayerEntry.getPlayerUsername() + "'s Rep: " 
+                        + ChatColor.LIGHT_PURPLE + String.format("%.1f", targetPlayerEntry.getRepShown()) 
+                        + ChatColor.DARK_GRAY + " (avg of " + targetPlayerEntry.getRepCount() + " entries)");
+                } catch (NumberFormatException e) {
+                    sender.sendMessage(prefix + ChatColor.RED + "The reputation value must be between 0-10 (or 'unset')!");
+                    return true;
+                }
             }
         }
-        
         return true;
     }
 
